b9240617c21ee5bbc03998ea4a918c56dda1ca7c
THWU <grinti0730@gmail.com>
2016-01-12 22:10:15 +0800

    Class 2

--- Practice/Class2/js/object.js
+++ Practice/Class2/js/object.js
@@ -59,3 +59,96 @@ function Persion(name, phone, isHappy) {
         return "人姓名:" + this.name + " 人電話:" + this.phone;
     }
 }
+
+// Class 2
+function A(a) {
+    this.varA = a;
+}
+
+A.prototype = {
+    varA: null,
+    doSomething: function() {
+        
+    }
+}
+
+/*
+function B(a, b) {
+    this.varA = a;
+    this.varB = b;
+}
+
+B.prototype = {
+    varA: null,
+    varB: null,
+    doSomething: function() {
+        
+    }
+}
+*/
+
+//  建構式
+function B(a, b) {
+    A.call(this, a);
+    this.varB = b;
+}
+
+B.prototype = {
+    varB: null,
+    doSomething: function() {
+        A.prototype.doSomething.apply(this, args);
+    }
+}
+
+// Example
+var a = 1, b = 3, c = 5;
+function add(a, b, c) {
+    return a + b + c;    
+}
+
+add.call(this, a, b, c);
+
+add.apply(this, [a, b, c]);
+
+add(1, 3, 5);
+
+add.call(this, 1, 3, 5);
+
+add.apply(this, [1, 3, 5]);
+
+//  Practice
+function Employee(name, baseSalary) {
+    this.name = name;
+    this.baseSalary = baseSalary;
+}
+
+Employee.prototype.getSalary = function () {
+    return this.baseSalary;
+};
+
+var resnick = new Employee("Yuan-Hung", 22000);
+console.log(resnick.getSalary());
+
+function Sales(name, baseSalary, performance) {
+    Employee.call(this, name, baseSalary);
+    this.performance = performance;
+}
+
+Sales.prototype.getSalary = function () {
+    return Employee.prototype.getSalary.apply(this) + (this.performance * 0.03);    
+};
+
+function Manager(name, baseSalary, performance) {
+    Employee.call(this, name, baseSalary);
+    this.performance = performance;
+}
+
+Manager.prototype.getSalary = function () {
+    return Employee.prototype.getSalary.apply(this) + (this.performance)
+};
+
+var rick = new Sales("Rick", 22000, 45000);
+console.log(rick.getSalary());
+
+var jacky = new Manager("Jacky", 22000, 10000);
+console.log(jacky.getSalary());
---
+++ Practice/Class2/practice_1/Paint.js
@@ -0,0 +1,131 @@
+﻿
+var myCtx;
+var myContext;
+var isDrawing = false;
+
+window.onload = function() {
+    
+    myCtx = new Ctx("drawingCanvas");
+    myCtx.getObj();
+    myContext = myCtx.getContext2d();
+    //  Start Drawing 畫布在滑鼠左鍵壓下的時候
+    myCtx.convas.onmousedown = startDrawing;
+    myCtx.convas.onmouseup = stopDrawing;          //  Stop Drawing
+    myCtx.convas.onmouseout = stopDrawing;         //  Stop Drawing
+    myCtx.convas.onmousemove = draw;               //  Draw
+};
+
+function Ctx(id) {
+    this.id = id;
+    this.canvas = null;
+}
+
+Ctx.prototype.getObj = function() {
+    this.canvas = document.getElementById(this.id);
+}
+
+Ctx.prototype.getContext2d = function() {
+    
+    if (this.canvas !== null) {
+        if (this.canvas.getContext) {
+            return this.canvas.getContext('2d');
+        }
+    }else {
+        alert("Can't find Canvas");
+    }
+}
+
+function startDrawing(e) {
+    
+    isDrawing = true;
+    myContext.beginPath();
+    var mouseX = e.pageX - myCtx.canvas.offsetLeft;
+    var mouseY = e.pageY - myCtx.canvas.offsetTop;
+    myContext.moveTo(mouseX, mouseY);
+}
+
+function stopDrawing(e) {
+    
+    isDrawing = false;
+    myContext.beginPath();
+    var mouseX = e.pageX - myCtx.canvas.offsetLeft;
+    var mouseY = e.pageY - myCtx.canvas.offsetTop;
+    myContext.moveTo(mouseX, mouseY);
+}
+
+function draw() {
+    
+    if (isDrawing) {
+        
+        var x = e.pageX - myCtx.canvas.offsetLeft;
+        var y = e.pageY - myCtx.canvas.offsetTop;
+        myContext.lineTo(x, y);
+        myContext.stroke();
+    }
+}
+
+
+
+function draw1() {
+    
+    myContext.fillRect(25,25,100,100);
+    myContext.clearRect(45,45,60,60);
+    myContext.strokeRect(50,50,50,50);
+}
+
+function draw2() {
+    
+    myContext.beginPath();
+    myContext.moveTo(75,50);
+    myContext.lineTo(100,75);
+    myContext.lineTo(100,25);
+    myContext.fill();
+}
+
+//  Change Color
+var previousColorElement;
+function changeColor(color, imgElement) {
+    
+    myContext.strokeStyle = color;
+    imgElement.className = "Selected";
+    
+    if (previousColorElement !== null) {
+        previousColorElement.className = "";
+    }
+    previousColorElement = imgElement;
+}
+
+//  Change Thickness
+var previousThicknessElement;
+function changeThickness(thickness, imgElement) {
+    
+    myContext.strokeStyle = thickness;
+    imgElement.className = "Selected";
+    
+    if (previousColorElement !== null) {
+        previousColorElement.className = "";
+    }
+    previousThicknessElement = imgElement; 
+}
+
+function clearCanvas() {
+    myContext.clearRect(0, 0, myCtx.convas.width, myCtx.convas.heigh);
+}
+
+function savaCanvas() {
+    
+    var imageCopy = document.getElementById('savedImageCopy');
+    imageCopy.src = myContext.toDataURL();
+    var imageContainer = document.getElementsById('savedCopyContainer');
+    imageContainer.style.display = 'block';
+}
+
+/*
+var canvas = document.getElementById('drawingCanvas');
+if (canvas.getContext) {
+    
+    var ctx = canvas.getContext('2d');    
+    //繪製圖形的程式碼寫在這邊
+    
+} // close if
+*/
