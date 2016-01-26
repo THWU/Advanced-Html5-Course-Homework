42a23f1d76c743613a44c3c6abcca3a0bdd2a8da
THWU <grinti0730@gmail.com>
2016-01-26 22:04:33 +0800

    update sticky animate

--- Courses/Course5/practice/stickyheader - 2/js/main.js
+++ Courses/Course5/practice/stickyheader - 2/js/main.js
@@ -1,1 +1,22 @@
-
+$(function() {
+   
+   $('.page-header').each(function() {
+      
+      var $window = $(window),
+          $header = $(this),
+          $headerClone = $header.contents().clone(),
+          $headerCloneContainer = $('<div class="page-header-clone"></div>');
+          
+      $headerCloneContainer.append($headerClone);
+      $headerCloneContainer.appendTo('body');
+      
+      var threshold = $header.offset().top + $header.outerHeight();
+      
+      $window.on('scroll', $.throttle(1000/15, function() {
+          
+          $window.scrollTop() > threshold? $headerCloneContainer.addClass('visible'): $headerCloneContainer.removeClass('visible');
+      }));
+      
+      $window.trigger('scroll');
+   });
+});
